2c01f7bcf26d576b0738c7a13c9204d7fd8e216e
biagolini <c.biagolini@gmail.com>
2023-05-26 10:10:29 -0300

    Fix safe access to aws_security_group

--- aws_security_group.tf
+++ aws_security_group.tf
@@ -2,7 +2,7 @@
 # Create security gropus for the current VPC
 #
 resource "aws_security_group" "security_group" {
-  for_each = { for sg in var.vpc_config.security_groups : sg.name => sg }
+  for_each = var.vpc_config.security_groups != null ? { for sg in var.vpc_config.security_groups : sg.name => sg } : {}
 
   name        = each.value.name
   description = each.value.description
