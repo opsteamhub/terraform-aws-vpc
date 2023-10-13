932cbe18374c5eeb2668c39bf56b8e9ccf25d562
biagolini <c.biagolini@gmail.com>
2023-10-13 12:04:46 -0300

    Fix empty security group

--- aws_security_group_rule.tf
+++ aws_security_group_rule.tf
@@ -3,18 +3,20 @@
 #
 resource "aws_security_group_rule" "ingress_rules" {
   for_each = {
-    for item in flatten([
-      for sg in var.vpc_config.security_groups :
-      sg.ingress != null ? [
-        for rule in sg.ingress : {
-          sg_name     = sg.name
-          from_port   = rule.from_port
-          to_port     = rule.to_port
-          protocol    = rule.protocol
-          cidr_blocks = rule.cidr_blocks
-        }
+    for item in flatten(
+      var.vpc_config.security_groups != null ? [
+        for sg in var.vpc_config.security_groups :
+        sg.ingress != null ? [
+          for rule in sg.ingress : {
+            sg_name     = sg.name
+            from_port   = rule.from_port
+            to_port     = rule.to_port
+            protocol    = rule.protocol
+            cidr_blocks = rule.cidr_blocks
+          }
+        ] : []
       ] : []
-    ]) : "${item.sg_name}-${item.from_port}-${item.to_port}" => item
+    ) : "${item.sg_name}-${item.from_port}-${item.to_port}" => item
   }
 
   type        = "ingress"
@@ -31,18 +33,20 @@ resource "aws_security_group_rule" "ingress_rules" {
 #
 resource "aws_security_group_rule" "egress_rules" {
   for_each = {
-    for item in flatten([
-      for sg in var.vpc_config.security_groups :
-      sg.egress != null ? [
-        for rule in sg.egress : {
-          sg_name     = sg.name
-          from_port   = rule.from_port
-          to_port     = rule.to_port
-          protocol    = rule.protocol
-          cidr_blocks = rule.cidr_blocks
-        }
+    for item in flatten(
+      var.vpc_config.security_groups != null ? [
+        for sg in var.vpc_config.security_groups :
+        sg.egress != null ? [
+          for rule in sg.egress : {
+            sg_name     = sg.name
+            from_port   = rule.from_port
+            to_port     = rule.to_port
+            protocol    = rule.protocol
+            cidr_blocks = rule.cidr_blocks
+          }
+        ] : []
       ] : []
-    ]) : "${item.sg_name}-${item.from_port}-${item.to_port}" => item
+    ) : "${item.sg_name}-${item.from_port}-${item.to_port}" => item
   }
 
   type        = "egress"
