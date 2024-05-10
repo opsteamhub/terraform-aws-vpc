1841ad7f4083c424adcd65ceb5cb33ffadcde57c
Bruno Paiuca <bruno.paiuca@ops.team>
2024-05-10 16:21:45 -0300

    hotfix/SG config - SG mergind VPC Global configs

--- aws_security_group.tf
+++ aws_security_group.tf
@@ -2,7 +2,19 @@
 # Create security gropus for the current VPC
 #
 resource "aws_security_group" "security_group" {
-  for_each = var.vpc_config.security_groups != null ? { for sg in var.vpc_config.security_groups : sg.name => sg } : {}
+  for_each = var.vpc_config.security_groups != null ? {
+    for sg in var.vpc_config.security_groups:
+      sg.name => (
+        var.vpc_config["vpc"]["vpc_id"] != null ? merge(
+          tomap(
+            {
+              vpc_id = var.vpc_config["vpc"]["vpc_id"]
+            }
+          ),
+          sg
+        ) : sg
+      )
+    } : {}
 
   name        = each.value.name
   description = each.value.description
